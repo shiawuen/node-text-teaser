33efd30bd73c2ec086419ba2d3efcf396174c795
shiawuen <shiawuen@gmail.com>
2014-12-11 14:00:40 +0800

    use request instead of superagent

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var debug = require('debug')('textteaser')
 var inspect = require('util').inspect
 var merge = require('merge')
 var path = require('path')
-var request = require('superagent')
+var request = require('request')
 
 module.exports = TextTeaser
 
@@ -21,17 +21,13 @@ TextTeaser.prototype.post = function(data, next){
   data = merge({ user: this.user }, data)
   debug('sending request to %s', url)
   debug('request data consists %s', Object.keys(data))
-  request
-    .post(url)
-    .type('form')
-    .send(data)
-    .end(function(err, res){
-      if (err) return next(err)
-      if (res.status >= 500)
-        return next(new Error(res.status + ':' + res.text))
-      debug('post response body', res.body)
-      next(null, res.body.id)
-    })
+  request.post({ url: url, form: data }, function(err, res, body){
+    if (err) return next(err)
+    if (res.statusCode >= 400)
+      return next(new Error(res.statusCode + ':' + res.text))
+    try { body = JSON.parse(body) } catch(e) {}
+    next(null, body.id)
+  })
 }
 
 TextTeaser.prototype.get = function get(id, opts, next){
@@ -43,21 +39,18 @@ TextTeaser.prototype.get = function get(id, opts, next){
 
   debug('sending GET request to %s, with query %s', url, inspect(opts))
 
-  request
-    .get(url)
-    .query(opts)
-    .end(function(err, res){
+  request({ url: url, qs: opts, json: true }, function(err, res, body){
       if (err) return next(err)
-      if (res.status >= 500)
-        return next(new Error(res.status + ':' + res.text))
+      if (res.statusCode >= 400)
+        return next(new Error(res.statusCode + ':' + res.text))
 
-      res.body = res.body || {}
+      body = body || {}
 
       var hasContent = true
-      hasContent &= !!Object.keys(res.body).length
-      hasContent &= res.body.status && 'pending' != res.body.status.toLowerCase()
+      hasContent &= !!Object.keys(body).length
+      hasContent &= body.status && 'pending' != body.status.toLowerCase()
 
-      if (hasContent) return next(null, res.body)
+      if (hasContent) return next(null, body)
 
       // summarization not ready yet
       var getAgain = get.bind(tt)
